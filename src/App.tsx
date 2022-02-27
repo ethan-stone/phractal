7780f535d23ca47b7d7208be51b9b689cf175bb7
ethan-stone <ethan.stone9352@gmail.com>
2022-02-27 02:14:33 -0500

    add temp workaround since build was failing - only using Auth from amplify so should probably just write a wrapper around amazon-cognito-identity-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Amplify from "aws-amplify";
+import Amplify from "@aws-amplify/core";
 import AuthContext from "./context/AuthContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RequireAuth from "./features/auth/RequireAuth";
