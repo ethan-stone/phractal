731edd49d440cb004e04e202cbed8f6123f83ad1
ethan-stone <ethan.stone9352@gmail.com>
2022-07-05 22:53:23 -0400

    feat: added auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { withTRPC } from "@trpc/next";
 import { AppRouter } from "./api/trpc/[trpc]";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
 
 export default withTRPC<AppRouter>({
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,19 @@ import { useState } from "react";
 import Editor from "../components/Editor";
 import Preview from "../components/Preview";
 import { trpc } from "../utils/trpc";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { status } = useSession({ required: true });
   const [previewOn, setPreviewOn] = useState<boolean>(false);
   const [doc, setDoc] = useState<string>("");
+
+  if (status === "loading")
+    return (
+      <div className="flex flex-col min-h-screen bg-neutral-800">
+        <div className="flex grow">Loading</div>
+      </div>
+    );
 
   return (
     <div className="flex flex-col min-h-screen bg-neutral-800">
