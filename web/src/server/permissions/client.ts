94b601be0c4440dff1dcb8abd3efdaa36cd0296c
ethan-stone <ethan.stone9352@gmail.com>
2023-04-21 08:30:37 -0400

    played around with permit more. solution is to create seperate roles per note

--- web/src/server/permissions/client.ts
+++ web/src/server/permissions/client.ts
@@ -6,23 +6,38 @@ export const client = new Permit({
 });
 
 async function main() {
-  const res1 = await client.api.updateResource("note_123", {
-    actions: {
-      read: {},
-      write: {},
-      delete: {},
-    },
+  // const res1 = await client.api.createResource({
+  //   key: "note_456",
+  //   name: "Note 456",
+  //   actions: {
+  //     read: {},
+  //     write: {},
+  //     delete: {},
+  //   },
+  // });
+  // console.log(res1);
+  // const res2 = await client.api.createRole({
+  //   key: "owner_456",
+  //   name: "Owner of 123",
+  //   permissions: ["note_456:read", "note_456:write", "note_456:delete"],
+  // });
+  // console.log(res2);
+  // const user = await client.api.createUser({
+  //   key: "user_1",
+  // });
+  // console.log(user);
+  const assign = await client.api.assignRole({
+    role: "editor_123",
+    user: "user_1",
+    tenant: "default",
   });
+  console.log(assign);
+  const start = new Date().getTime();
+  const check = await client.check("user_1", "read", "note_123");
+  const end = new Date().getTime();
 
-  console.log(res1);
-
-  const res2 = await client.api.createRole({
-    key: "owner_123",
-    name: "Owner of 123",
-    permissions: ["note_123:read", "note_123:write", "note_123:delete"],
-  });
-
-  console.log(res2);
+  console.log(check);
+  console.log(end - start);
 }
 
 void main();
