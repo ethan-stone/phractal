13d57f21a9fb585640d4938f4a4407e361ff1ff3
ethan-stone <ethan.stone9352@gmail.com>
2023-05-02 22:01:05 -0400

    add icon and style changes to editor

--- web/src/pages/notes/[noteId].tsx
+++ web/src/pages/notes/[noteId].tsx
@@ -9,6 +9,7 @@ import { api } from "@/utils/api";
 import { LiveObject } from "@liveblocks/client";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 const Editor: React.FC = ({}) => {
   const content = useStorage((root) => root.note.content);
@@ -25,7 +26,7 @@ const Editor: React.FC = ({}) => {
 
   if (content === null) {
     return (
-      <div className="flex h-5/6 w-3/4 max-w-3xl flex-grow resize-none rounded border border-neutral-900 bg-neutral-200 p-8 font-mono text-neutral-900 shadow-2xl focus:outline-none">
+      <div className="flex h-full w-3/4 max-w-3xl flex-grow resize-none rounded border border-neutral-900 bg-neutral-200 p-8 font-mono text-neutral-900 shadow-2xl focus:outline-none">
         <Spinner />
       </div>
     );
@@ -34,7 +35,7 @@ const Editor: React.FC = ({}) => {
   return (
     <textarea
       value={content}
-      className="flex h-5/6 w-3/4 max-w-3xl flex-grow resize-none rounded border border-neutral-900 bg-neutral-200 p-8 font-mono text-neutral-900 shadow-2xl focus:outline-none"
+      className="flex h-5/6 max-w-6xl flex-grow resize-none rounded-2xl bg-neutral-200 p-8 font-mono text-neutral-900 shadow-inner focus:outline-none"
       onClick={(e) => {
         const cursorPosition = e.currentTarget.selectionStart;
         updateMyPresence({
@@ -81,6 +82,43 @@ const Editor: React.FC = ({}) => {
   );
 };
 
+const NameAndDescription: React.FC = () => {
+  const name = useStorage((root) => root.note.name);
+
+  const updateNote = useMutation(
+    ({ storage }, noteType: "content" | "name", newNote: string) => {
+      const mutableNote = storage.get("note");
+      mutableNote.set(noteType, newNote);
+    },
+    []
+  );
+
+  if (name === null) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-full flex-col px-8 py-24">
+      <input
+        className="border border-none p-4 font-mono text-3xl outline-none"
+        placeholder="Untitled"
+        value={name}
+        onChange={(e) => {
+          updateNote("name", e.target.value);
+        }}
+      />
+      <button className="flex w-full flex-row items-center justify-center gap-4 rounded bg-neutral-900 p-3 font-mono text-white">
+        <ArrowPathIcon height={20} width={20} />
+        Generate Summary
+      </button>
+    </div>
+  );
+};
+
 const Note: NextPage = () => {
   const router = useRouter();
 
@@ -121,13 +159,7 @@ const Note: NextPage = () => {
       }}
     >
       <main className="flex h-screen flex-row items-center justify-center bg-white">
-        <div className="flex flex-grow flex-col items-center justify-center p-8">
-          <input
-            className="border border-none p-4 font-mono text-3xl outline-none"
-            placeholder="Untitled"
-          />
-          <button className="font-mono">Generate Summary</button>
-        </div>
+        <NameAndDescription />
         <div className="flex h-full w-full flex-grow items-center justify-center">
           <Editor />
         </div>
